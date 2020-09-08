055d828427b73be6268fce325a503fb9b81ed962
Mathias Azzolini <mathiasa@dbserver.com.br>
2020-09-07 22:53:21 -0300

    fix second change

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ console.log('Just a console');
 console.log('A new feature for one');
 
 
-console.log('my new line feature two, but with my correction');
+console.log('my new line feature two, but with my correction and my second change');
